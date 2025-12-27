e0f2920f1d953c64a0fe484064f4880e76d415f5
dan <you@example.com>
2025-12-27 17:22:09 -0300

    fix cookies aaaa

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import os
 async def get_vid(video):
     if os.path.isfile("config/cookies.txt"):
         print("cookie")
-        return subprocess.check_output(["yt-dlp", "--cookies", "cookies.txt", "--no-warnings", "-f", "best/bestvideo+bestaudio", "--get-url", video])
+        return subprocess.check_output(["yt-dlp", "--cookies", "config/cookies.txt", "--no-warnings", "-f", "best/bestvideo+bestaudio", "--get-url", video])
     else:
         return subprocess.check_output(["yt-dlp", "--no-warnings", "-f", "best/bestvideo+bestaudio", "--get-url", video])
 
